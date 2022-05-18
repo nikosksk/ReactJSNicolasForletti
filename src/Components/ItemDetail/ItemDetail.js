7c20f36fb03d63fa52529823e542ba8fd482b4c4
nikosksk <nicoforletti@gmail.com>
2022-05-18 20:10:27 -0300

    commit4

--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -1,15 +1,16 @@
-
 import React from 'react'
 
 const ItemDetail = ({data}) => {
   return (
     <div className='container'>
         <div className='detail'>
-            <img className='detail_image' src="*" alt="*">
+            <img className='detail_image' 
+                 src='#' 
+                 alt='imagen'
+                 />
                 <div className='container'>
                     <h1>{data.title}</h1>
                 </div>
-            </img>
         </div>
     </div>
   )
